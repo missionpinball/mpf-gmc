31624889d60d759f47a9633f746f72819ec1e2e0
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-03-28 11:30:43 -0700

    Support event kwargs in MPFVariable values

--- addons/mpf-gmc/classes/mpf_variable.gd
+++ addons/mpf-gmc/classes/mpf_variable.gd
@@ -1,7 +1,7 @@
 @tool
 class_name MPFVariable extends Label
 
-@export_enum("Current Player", "Machine", "Player 1", "Player 2", "Player 3", "Player 4") var variable_type: String = "Current Player"
+@export_enum("Current Player", "Machine", "Event Arg", "Player 1", "Player 2", "Player 3", "Player 4") var variable_type: String = "Current Player"
 @export var variable_name: String
 @export var comma_separate: bool
 @export var min_digits: int = -1
@@ -13,21 +13,34 @@ func _ready() -> void:
     if min_digits > 0:
       var_template = ("%0"+str(min_digits)+"d")
     if variable_type == "Machine":
-        self._set_text(MPF.game.machine_vars.get(self.variable_name))
+        self.update_text(MPF.game.machine_vars.get(self.variable_name))
         # TODO: Dynamically update machine vars?
+    elif variable_type == "Event Arg":
+        # No initialization, use the initialize() method instead
+        return
     else:
         var player_num = int(variable_type.right(1))
         var is_current_player = variable_type == "Current Player" or player_num == MPF.game.player.get('number')
         if is_current_player:
             MPF.game.connect("player_update", self._on_player_update)
-            self._set_text(MPF.game.player.get(self.variable_name))
+            self.update_text(MPF.game.player.get(self.variable_name))
         elif MPF.game.players.size() >= player_num:
-            self._set_text(MPF.game.players[player_num - 1].get(self.variable_name))
+            self.update_text(MPF.game.players[player_num - 1].get(self.variable_name))
 
-func _set_text(value):
+func update(settings: Dictionary, kwargs: Dictionary = {}) -> void:
+    if variable_type != "Event Arg":
+        return
+    # The value may be passed via the tokens: config
+    if variable_name in settings.get("tokens", {}):
+        self.update_text(settings.tokens[variable_name])
+    # Or the value may be part of the triggering event
+    elif variable_name in kwargs:
+        self.update_text(kwargs[variable_name])
+
+func update_text(value):
     if not value:
         self.text = ""
-    if value is int:
+    if value is int or value is float:
         if comma_separate and value >= 1000:
             value = MPF.game.comma_sep(value)
         else:
@@ -39,4 +52,4 @@ func _set_text(value):
 
 func _on_player_update(var_name, value):
     if var_name == variable_name:
-        self._set_text(value)
+        self.update_text(value)
