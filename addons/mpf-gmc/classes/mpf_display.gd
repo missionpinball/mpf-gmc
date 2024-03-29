ce64447f996692d7a2aee97103c869e8f65b0512
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-03-29 15:29:05 -0700

    Support for slide expire config

--- addons/mpf-gmc/classes/mpf_display.gd
+++ addons/mpf-gmc/classes/mpf_display.gd
@@ -5,6 +5,7 @@ class_name MPFDisplay extends Node2D
 @export var is_default: bool = false
 @export var initial_slide: PackedScene
 var _slide_stack = []
+var _timers = []
 
 func _ready() -> void:
     if not self.initial_slide:
@@ -19,11 +20,12 @@ func process_slide(slide_name: String, action: String, settings: Dictionary, con
     for s in _slide_stack:
         if s.key == slide_name:
             slide = s
+            break
     match action:
         "play":
             # Don't play a slide that's already there
             if not slide:
-                self.play_slide(slide_name, settings, context, priority, kwargs)
+                slide = self.play_slide(slide_name, settings, context, priority, kwargs)
         "remove":
             if slide:
                 self.remove_slide(slide)
@@ -34,15 +36,23 @@ func process_slide(slide_name: String, action: String, settings: Dictionary, con
             if slide and slide.has_method(settings.method):
                 var callable = Callable(slide, settings.method)
                 callable.call(settings, kwargs)
+    if slide and settings.expire:
+        var timer = Timer.new()
+        timer.wait_time = float(settings.expire)
+        timer.one_shot = true
+        timer.autostart = true
+        timer.timeout.connect(self._on_expire.bind(slide, timer))
+        self.add_child(timer)
 
 
-func play_slide(slide_name: String, settings: Dictionary, context: String, priority: int = 0, kwargs: Dictionary = {}) -> void:
+func play_slide(slide_name: String, settings: Dictionary, context: String, priority: int = 0, kwargs: Dictionary = {}) -> MPFSlide:
     var slide = MPF.mc.get_slide(slide_name)
     assert(slide is MPFSlide, "Slide scenes must use the MPFSlide script on the root node.")
     slide.initialize(slide_name, settings, context, priority, kwargs)
     self._slide_stack.append(slide)
     self.add_child(slide)
     self._update_stack()
+    return slide
 
 func remove_slide(slide) -> void:
     self._slide_stack.erase(slide)
@@ -69,3 +79,7 @@ func _on_clear(mode_name) -> void:
     # TODO: For the remaining slides, call clear to remove any
     # widgets added by the cleared mode
     self._update_stack()
+
+func _on_expire(slide: MPFSlide, timer: Timer) -> void:
+    self.remove_child(timer)
+    self.remove_slide(slide)
