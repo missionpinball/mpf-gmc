a4212ffd5e43a6b4e0e79e73881b06e3df081d8e
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-05-17 12:56:01 -0700

    Docstring comments for DuckSettings properties

--- addons/mpf-gmc/classes/mpf_ducking.gd
+++ addons/mpf-gmc/classes/mpf_ducking.gd
@@ -5,17 +5,25 @@ class_name DuckSettings
 const merge_props = ["delay", "attenuation", "attack", "release", "release_from_start", "release_point"]
 
 var bus: GMCBus
+## The name of the audio bus to duck
 @export var target_bus: String:
 	set = _set_target_bus
+## A delay (seconds) before the ducking attack
 @export var delay: float
+## The volume (in db) to reduce the target bus by
 @export var attenuation: float
+## The duration (seconds) over which to reduce the target bus value
 @export var attack: float
+## The duration (seconds) over which to restore the target bus value
 @export var release: float
+## The time (seconds) at which the ducking should begin releasing (from the start of the sound)
 @export var release_from_start: float
+## The time (seconds) at which the ducking should begin releasing (from the end of the sound)
 @export var release_point: float
 var start_time
 var duration: float
 var release_time: int
+
 
 func _init(settings: Dictionary = {}, fallback: DuckSettings = null):
 	# The bus comes as a string, convert it to a GMCBus
@@ -42,7 +50,8 @@ func calculate_release_time(start_time_msecs: int, stream: AudioStream = null) -
 func _set_target_bus(value: String) -> void:
 	target_bus = value
 	# The saved bus may have been overridden by _init settings
-	if not bus:
+	# Bind to the bus, but not in editor because it validates on every keystroke
+	if not bus and not Engine.is_editor_hint():
 		bus = MPF.media.sound.get_bus(value)
 
 func _to_string() -> String:
