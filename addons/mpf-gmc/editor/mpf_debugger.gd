f4d24fdc991af04ca3ab210a7e511780bd97ebab
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-05-29 16:15:12 -0700

    Working MPF debugger all the time!

--- addons/mpf-gmc/editor/mpf_debugger.gd
+++ addons/mpf-gmc/editor/mpf_debugger.gd
@@ -1,6 +1,5 @@
 @tool
 ## A plugin to attach the Debugger session to the MPF Output Panel
-class_name MPFDebuggerPlugin
 extends EditorDebuggerPlugin
 
 var editor_panel
@@ -9,9 +8,23 @@ func _has_capture(prefix):
 	# Return true if you wish to handle message with this prefix.
 	return prefix == "mpf_log_created"
 
-func _capture(message, data, session_id):
+func _capture(message, data, _session_id):
 	if editor_panel:
-		editor_panel._on_log_created(message, data)
+		editor_panel.open_log_message(message, data)
+	return true
+
+func _setup_session(session_id: int) -> void:
+	var session = self.get_session(session_id)
+	session.started.connect(self._on_session_started)
+	session.stopped.connect(self._on_session_stopped)
+
+func _on_session_started():
+	if editor_panel:
+		editor_panel.session_started()
+
+func _on_session_stopped():
+	if editor_panel:
+		editor_panel.session_stopped()
 
 func attach_panel(panel: Object):
 	editor_panel = panel
--- addons/mpf-gmc/editor/mpf_output.gd
+++ addons/mpf-gmc/editor/mpf_output.gd
@@ -4,14 +4,15 @@ extends Control
 var log_file: FileAccess
 var log_file_path
 var time := 0.0
+var is_stopping: bool = false
 
 @export var terminal: TextEdit
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	push_warning("MPF OUTPUT terminal %s is ready, clearing text." % self)
 	terminal.text = "No MPF Log Data."
+	set_process(false)
 
 func _exit_tree() -> void:
 	log_file_path = null
@@ -19,12 +20,19 @@ func _exit_tree() -> void:
 		log_file.close()
 		log_file = null
 
-func _on_log_created(message: String, data: Array) -> bool:
-	push_warning("MPF OUTPUT terminal %s loading log file at %s" % [self, data[0]])
+func session_started() -> void:
+	terminal.text = ""
+	self.log_file = null
+	self.log_file_path = null
+
+func session_stopped() -> void:
+	self.is_stopping = true
+
+func open_log_message(_message: String, data: Array):
 	terminal.text = ""
 	self.log_file = null
 	self.log_file_path = data[0]
-	return true
+	set_process(true)
 
 func _process(delta: float) -> void:
 	time += delta
@@ -32,10 +40,12 @@ func _process(delta: float) -> void:
 		return
 
 	if log_file:
-		#push_warning(" - log file position is %s out of length %s" % [log_file.get_position(), log_file.get_length()])
 		while log_file.get_position() < log_file.get_length():
 			var next_line = log_file.get_line()
 			terminal.insert_text_at_caret("%s\n" % next_line)
+		if is_stopping:
+			set_process(false)
+			is_stopping = false
 		time = 0.0
 	elif log_file_path:
 		if time > 1:
@@ -46,7 +56,6 @@ func _process(delta: float) -> void:
 
 
 func _open_log() -> void:
-
 	log_file = FileAccess.open(log_file_path, FileAccess.READ)
 	if not log_file:
 		var err = FileAccess.get_open_error()
