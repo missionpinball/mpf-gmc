3a340aa5cc7813a50adfd9a9313bd4972a264639
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-06-12 13:03:55 -0700

    Explicit time stamping, not using _process()

--- addons/mpf-show-creator/classes/MPFShowCreator.gd
+++ addons/mpf-show-creator/classes/MPFShowCreator.gd
@@ -20,8 +20,6 @@ const CONFIG_PATH = "user://mpf_show_creator.cfg"
 @export var light_groups: String = ""
 
 var lights = []
-var time = 0.0
-var cum_time = 0.0
 var spf: float
 var file: FileAccess
 var file_path: String
@@ -56,9 +54,9 @@ func _ready():
 
 	if not self.lights:
 		if self._groups:
-			printerr("No lights found matching the selected groups.")
+			assert(false, "No lights found matching the selected groups.")
 		else:
-			printerr("No lights found. Please add some MPFShowLight nodes.")
+			assert(false, "No lights found. Please add some MPFShowLight nodes.")
 	self.spf = 1.0 / self.fps
 	self.clip_children = CanvasItem.CLIP_CHILDREN_ONLY
 
@@ -67,22 +65,24 @@ func _ready():
 	self.file.store_line("#show_version=6")
 
 	await RenderingServer.frame_post_draw
-	self.snapshot(true)
+	self.animation_player.assigned_animation = animation_name
+	self.animation_player.callback_mode_process = AnimationPlayer.ANIMATION_CALLBACK_MODE_PROCESS_MANUAL
 	self.animation_player.play(animation_name)
+	self.animation_player.advance(0)
 	self.animation_player.animation_finished.connect(self.on_animation_finished)
-	set_process(true)
+	self._run_animation()
 
-func _process(delta):
-	time += delta
-	cum_time += delta
-	if time < self.spf:
-		return
-	time = 0.0
-	self.snapshot()
+func _run_animation():
+	print("Generating animation show %s.yaml: duration %ss with frames every %0.5fs" % [self.animation_player.current_animation, self.animation_player.current_animation_length, self.spf])
+	var duration = self.animation_player.current_animation_length
+	while self.animation_player.current_animation_position <= duration:
+		await RenderingServer.frame_post_draw
+		self.snapshot()
+		self.animation_player.advance(self.spf)
 
 func register_light(light: MPFShowLight):
 	if light.position.x < 0 or light.position.y < 0 or light.position.x > self.texture.get_width() or light.position.y > self.texture.get_height():
-		printerr("Light %s is outside of the viewport and will not be included." % self.name)
+		push_warning("Light %s is outside of the viewport and will not be included." % light.name)
 		return
 	if self._groups:
 		var has_match = false
@@ -96,21 +96,23 @@ func register_light(light: MPFShowLight):
 
 func snapshot(is_initial=false):
 	var tex := get_viewport().get_texture().get_image()
+	var timestamp = self.animation_player.current_animation_position
 	var light_lines := []
 	for l in lights:
 		var c = l.get_color(tex, strip_unchanged_lights)
 		if c != null:
 			light_lines.append("    %s: \"%s\"" % [l.name, c.to_html(use_alpha)])
 	if light_lines or not strip_empty_times:
-		if is_initial:
-			file.store_line("- time: 0.0")
-		else:
-			file.store_line("- time: %0.5f" % cum_time)
-		file.store_line("  lights:")
-		for line in light_lines:
-			file.store_line(line)
+		file.store_line("- time: %0.5f" % timestamp)
+		if light_lines:
+			file.store_line("  lights:")
+			for line in light_lines:
+				file.store_line(line)
 
-func on_animation_finished(_animation_name):
+func on_animation_finished(_animation_name=null):
+	self.finish()
+
+func finish():
 	set_process(false)
 	file.close()
 	OS.shell_show_in_file_manager(self.file_path)
