3e44af71d4db56462cd88949f1c0af9473626fc3
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-06-12 14:00:49 -0700

    MPFShowLight use global position, update notification warning

--- addons/mpf-show-creator/classes/MPFShowLight.gd
+++ addons/mpf-show-creator/classes/MPFShowLight.gd
@@ -6,6 +6,7 @@ var current_color
 
 func _ready():
 	if Engine.is_editor_hint():
+		self.set_notify_transform(true)
 		return
 	var parent = self.get_parent()
 	while parent:
@@ -14,14 +15,18 @@ func _ready():
 		parent = parent.get_parent()
 
 func get_color(data: Image, suppress_unchanged: bool = false):
-	var color = data.get_pixelv(self.position)
+	var color = data.get_pixelv(self.global_position)
 	if color == current_color and suppress_unchanged:
 		return null
 	current_color = color
 	return color
 
 func _get_configuration_warnings():
-	if self.position == Vector2(-1, -1):
+	if self.global_position == Vector2(-1, -1):
 	# if self.position.x == 0 and self.position.y == 0:
 		return ["Light has not been positioned."]
 	return []
+
+func _notification(what):
+	if(what == NOTIFICATION_TRANSFORM_CHANGED):
+		self.update_configuration_warnings()
