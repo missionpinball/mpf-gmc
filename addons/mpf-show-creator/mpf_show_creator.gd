94823d4605f0d615fb3ab829a579661935101449
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-06-13 16:50:54 -0700

    Custom icons for show creator node classes

--- addons/mpf-show-creator/mpf_show_creator.gd
+++ addons/mpf-show-creator/mpf_show_creator.gd
@@ -8,8 +8,8 @@ func _enter_tree():
 	if version.major < 4 or version.minor < 2:
 		push_error("MPF Show Creator requires Godot version 4.2 or later, you have %s.%s.%s. Please visit godotengine.org to update." % [version.major, version.minor, version.patch])
 		return
-	add_custom_type("MPFShowCreator", "Sprite2D", preload("classes/MPFShowCreator.gd"), null)
-	add_custom_type("MPFShowLight", "Node2D", preload("classes/MPFShowLight.gd"), null)
+	add_custom_type("MPFShowCreator", "Sprite2D", preload("classes/MPFShowCreator.gd"), preload("icons/Camera2D.svg"))
+	add_custom_type("MPFShowLight", "Node2D", preload("classes/MPFShowLight.gd"), preload("icons/DirectionalLight2D.svg"))
 	show_creator_dock = preload("res://addons/mpf-show-creator/mpf_show_creator_dock.tscn").instantiate()
 	add_control_to_bottom_panel(show_creator_dock, "MPF Show Creator")
 
