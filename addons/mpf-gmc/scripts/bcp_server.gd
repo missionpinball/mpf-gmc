3767d51f37e03bd2170d833c82a83e9cd10e42b4
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-06-21 15:39:55 -0700

    BCP server handler for service_mode_entered

--- addons/mpf-gmc/scripts/bcp_server.gd
+++ addons/mpf-gmc/scripts/bcp_server.gd
@@ -347,8 +347,9 @@ func _thread_poll(_userdata=null) -> void:
 							_send("register_trigger?event=%s" % e)
 					"service":
 						call_deferred("emit_signal", "service", message)
-					# "service_mode_entered":
-					# 	call_deferred("deferred_scene", "res://modes/Service.tscn")
+					"service_mode_entered":
+						# The default service.yaml includes a slide_player for service slide
+						pass
 					"settings":
 						call_deferred("deferred_game", "update_settings", message)
 					"signal":
