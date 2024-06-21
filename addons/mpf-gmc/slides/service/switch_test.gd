91036389b9aec3f9cd970596718e9e04841e7ef2
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-06-20 20:05:20 -0700

    Better-ish UI for switch test

--- addons/mpf-gmc/slides/service/switch_test.gd
+++ addons/mpf-gmc/slides/service/switch_test.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends HBoxContainer
 
 func _ready() -> void:
 	MPF.server.service.connect(self._on_service)
@@ -14,23 +14,26 @@ func _on_service(payload: Dictionary) -> void:
 	self._get_active_switches()
 
 	var label: String = payload.switch_label.uri_decode()
-	$last_switch_label.text = label
-	$last_switch_address.text = payload.switch_num.uri_decode()
+	# If user-friendly labels are not provided, use the key name
+	if not label:
+		label = payload.switch_name
+	$active_test/last_switch_label.text = label
+	$active_test/last_switch_address.text = payload.switch_num.uri_decode()
 
 	var recency = Label.new()
 	var prefix: String
 	if payload.switch_state == "active":
 		prefix = "A"
-		$last_switch_state.text = "Active"
+		$active_test/last_switch_state.text = "Active"
 	else:
 		prefix = "I"
 		recency.modulate = Color(1,1,1,0.5)
-		$last_switch_state.text = "Inactive"
+		$active_test/last_switch_state.text = "Inactive"
 	recency.text = "[%s] %s" % [prefix, label]
-	$recent_switches.add_child(recency)
-	while $recent_switches.get_child_count() > 20:
-		var switch_to_remove = $recent_switches.get_child(0)
-		$recent_switches.remove_child(switch_to_remove)
+	$recent_test/recent_switches.add_child(recency)
+	while $recent_test/recent_switches.get_child_count() > 20:
+		var switch_to_remove = $recent_test/recent_switches.get_child(0)
+		$recent_test/recent_switches.remove_child(switch_to_remove)
 		switch_to_remove.queue_free()
 
 	# # HACK! Use the start button to exit out of this mode
@@ -41,17 +44,18 @@ func _on_service(payload: Dictionary) -> void:
 		get_tree().input_event(e)
 
 func _get_active_switches() -> void:
+	# TODO: Get switches that don't have labels
 	MPF.server.send_service("list_switches", ["label", "state"])
 
 func _update_active_switches(payload: Dictionary) -> void:
 	# Remove the existing children. Used to exclude first, now all.
-	while $active_switches.get_child_count() > 0:
-		var child_to_remove = $active_switches.get_child(0)
-		$active_switches.remove_child(child_to_remove)
+	while $active_test/active_switches.get_child_count() > 0:
+		var child_to_remove = $active_test/active_switches.get_child(0)
+		$active_test/active_switches.remove_child(child_to_remove)
 		child_to_remove.queue_free()
 
 	for switch in payload.switches:
 		if switch[1]:
 			var child = Label.new()
 			child.text = switch[0]
-			$active_switches.add_child(child)
+			$active_test/active_switches.add_child(child)
