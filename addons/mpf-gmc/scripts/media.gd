09eb10d7988ae6767edae3ca8affc8c4088fed9d
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-07-11 10:17:56 -0700

    Bugfix media content root config section "settings" becomes "gmc"

--- addons/mpf-gmc/scripts/media.gd
+++ addons/mpf-gmc/scripts/media.gd
@@ -82,7 +82,7 @@ func _get_scene(scene_name: String, collection: Dictionary, preload_only: bool =
 func traverse_tree_for(obj_type: String, acc: Dictionary, ext="tscn") -> void:
 	# Look for a specified content root
 	var content_root: String = "res://%s" % obj_type
-	if MPF.has_config_section("settings"):
+	if MPF.has_config_section("gmc"):
 		var root = MPF.get_config_value("gmc", "content_root", "")
 		if root:
 			content_root = "res://%s/%s" % [root, obj_type]
