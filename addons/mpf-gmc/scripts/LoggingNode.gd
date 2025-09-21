ffef11d9839d82cf955ea0707f684982838e7818
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2025-09-21 13:32:30 -0700

    Rename logging node to avoid Godot 4.5 native conflict

--- addons/mpf-gmc/scripts/LoggingNode.gd
+++ addons/mpf-gmc/scripts/LoggingNode.gd
@@ -7,8 +7,8 @@ extends Node
 
 @warning_ignore("shadowed_global_identifier")
 var log: GMCLogger
-const Logger = preload("res://addons/mpf-gmc/scripts/log.gd")
+const GLogger = preload("res://addons/mpf-gmc/scripts/log.gd")
 
 
-func configure_logging(log_name: String = self.name, level: Logger.LogLevel = Logger.LogLevel.USE_GLOBAL_LEVEL, set_global: bool = false):
-  self.log = Logger.new(log_name, level, set_global)
+func configure_logging(log_name: String = self.name, level: GLogger.LogLevel = GLogger.LogLevel.USE_GLOBAL_LEVEL, set_global: bool = false):
+  self.log = GLogger.new(log_name, level, set_global)
